6a44e14e11d315d3d3da513d1d6bf08f7254ea3b
VinayakAlagwadi <78849168+VinayakAlagwadi@users.noreply.github.com>
2021-03-08 01:09:18 +0530

    Update TeamDAO.java

--- MiniProject/src/com/ltts/Dao/TeamDAO.java
+++ MiniProject/src/com/ltts/Dao/TeamDAO.java
@@ -1,93 +1,67 @@
 package com.ltts.Dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import com.ltts.Configuration.MyConnection;
+import com.ltts.model.Player;
 
-import com.ltts.model.Team;
-
-
-public class TeamDAO {
-	public List<Team> getAllTeams(){
-		List<Team> li=new ArrayList<Team>();
+public class PlayerDAO {
+	public List<Player> getAllPlayers(){
+		List<Player> li=new ArrayList<Player>();
 		
 		return li;
 
 }
+public Player getplayerById(int id) {
+	Player p=new Player();
+	// p.getPlayerId()
 	
-public Team getTeamById(int id) {
-	Team t = new Team(id, null);
-	
-	return t;
+	return p;
 }
 
-public boolean createTeams() throws Exception {
-	Connection mc=MyConnection.getConnection();
+public boolean createPlayer() throws Exception {
+	Connection mc= MyConnection.getConnection();
 	
-	PreparedStatement ps=mc.prepareStatement("create table if not exists Team(teamId int(2) primary key , teamName varchar(20))");
+	PreparedStatement ps1=mc.prepareStatement("create table Player(playerId int(2) primary key , name varchar(20), date date, skill varchar(30), noOfmatches int, runs int, wickets int,nation varchar(30),power double,teamId int,FOREIGN KEY (teamId) REFERENCES Team(teamid))");
 	
 	//ps.setInt(1,getPlayerId());
 	//ps.setString(2,getPlayerName());
-	return ps.execute();
+	return ps1.execute();
 	
 }
 
-public void insertTeams() throws Exception {
+
+public void insertPlayer(Player p) throws Exception {
+	
+	//Player pla = new Player();
 	Connection mc=MyConnection.getConnection();
-	
-	Statement s = mc.createStatement();
-	//s.execute("insert into customer values ('Vishal','23')");
-	//s.executeUpdate("insert into Team values ((1,'CSK'),(2,'DC'),(3,'KKR'),(4,'KXIP'),(5,'MI'),(6,'RCB'),(7,'RR'),(8,'SRH')");
-	
-//	s.executeUpdate("insert into Team values (1,'CSK')");
-//	s.executeUpdate("insert into Team values (2,'DC')");
-//	s.executeUpdate("insert into Team values (3,'KKR')");
-//	s.executeUpdate("insert into Team values (4,'KXIP')");
-//	s.executeUpdate("insert into Team values (5,'MI')");
-//	s.executeUpdate("insert into Team values (6,'RCB')");
-//	s.executeUpdate("insert into Team values (7,'RR')");
-//	s.executeUpdate("insert into Team values (8,'SRH')");
-//	System.out.println("Record is inserted");
-//	
-//	ResultSet rs = s.executeQuery("select * from team");
-//	
-//	while(rs.next()){
-//		System.out.println(rs.getString(1)+" " + rs.getString(2));
-//	}
-//	
-	
-	PreparedStatement ps=mc.prepareStatement("insert into Team  (teamId,teamName) values (1,'CSK'),(2,'DC'),(3,'KKR'),(4,'KXIP'),(5,'MI'),(6,'RCB'),(7,'RR'),(8,'SRH')");
-	
-	//ResultSet rs = s.executeQuery("select * from Team");
-	
-	System.out.println("Record is inserted");
-	 ps.executeUpdate();
-	
-	//ps.setInt(1,getPlayUerId());
-	//ps.setString(2,getPlayerName());
-	//ResultSet rs =s.executeQuery(null);
-	//return s.execute(null);
+	Statement stmt = mc.createStatement();
+	PreparedStatement ps=mc.prepareStatement("insert into Player values(?,?,?,?,?,?,?,?,?,?);");
+	  ps.setInt(1,p.getPlayerId());
+	  ps.setString(2,p.getName()); ps.setString(3,p.getDateOfBirth());
+	  ps.setString(4,p.getSkill()); ps.setInt(5,p.getNumberOfMatches());
+	  ps.setInt(6,p.getRuns()); ps.setInt(7,p.getWickets());
+	  ps.setString(8,p.getNationality()); ps.setDouble(9,p.getPowerRating());
+	  ps.setInt(10,p.getTeamId());
+	 
+	 System.out.println(p.getName());
+	 ps.executeUpdate();	
+	 System.out.println("Successfully Added");
+		/*
+		 * String sql =
+		 * "INSERT INTO Player VALUES("+p.getPlayerId()+",'"+p.getName()+"','"+p.
+		 * getDateOfBirth()+"','"+p.getSkill()+"',"+p.getNumberOfMatches()+","+p.
+		 * getRuns()+","+p.getWickets()+",'"+p.getNationality()+"',"+p.
+		 * getPowerRating()+","+p.getTeamId()+")"; stmt.executeUpdate(sql);
+		 * System.out.println("susccefully");
+		 */
+		  
+		 
 	
 }
-
-
-
-
-
-private int getPlayerId() {
-	// TODO Auto-generated method stub
-	return 0;
-}
-private String getPlayerName() {
-	// TODO Auto-generated method stub
-	return null;
-}
-public boolean updatePlayer(int id) {
-	return true;
-}
-
 }
