09331bc6cf38a091dc28e90eb624ebedadd52882
VinayakAlagwadi <78849168+VinayakAlagwadi@users.noreply.github.com>
2021-03-08 01:11:15 +0530

    Update Player.java

--- MiniProject/src/com/ltts/model/Player.java
+++ MiniProject/src/com/ltts/model/Player.java
@@ -5,17 +5,14 @@ import java.util.Date;
 public class Player {
 	private int playerId;
 	private String name;
-	private Date dateOfBirth;
+	private String dateOfBirth;
 	private String skill;
 	private int numberOfMatches;
 	private int runs;
 	private int wickets;
 	private String nationality;
 	private double powerRating;
-	
-	
-	
-	
+	private int teamId;
 	
 	
 	
@@ -24,29 +21,38 @@ public class Player {
 	public Player() {
 		super();
 	}
-	public Player(int playerId, String name, Date dateOfBirth, String skill, int numberOfMatches, int runs, int wickets,
-			String nationality, double powerRating) {
+	
+	
+	
+	
+	public Player(int playerId, String name, String ppno, String skill, int numberOfMatches, int runs, int wickets,
+			String nationality, double powerRating, int teamId) {
 		super();
 		this.playerId = playerId;
 		this.name = name;
-		this.dateOfBirth = dateOfBirth;
+		this.dateOfBirth = ppno;
 		this.skill = skill;
 		this.numberOfMatches = numberOfMatches;
 		this.runs = runs;
 		this.wickets = wickets;
 		this.nationality = nationality;
 		this.powerRating = powerRating;
+		this.teamId = teamId;
 	}
+
+
+
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Date getDateOfBirth() {
+	public String getDateOfBirth() {
 		return dateOfBirth;
 	}
-	public void setDateOfBirth(Date dateOfBirth) {
+	public void setDateOfBirth(String dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
 	public String getSkill() {
@@ -90,6 +96,12 @@ public class Player {
 	}
 	public void setPlayerId(int playerId) {
 		this.playerId = playerId;
+	}
+	public int getTeamId() {
+		return teamId;
+	}
+	public void setTeamId(int teamId) {
+		this.teamId = teamId;
 	}
 	
 	
